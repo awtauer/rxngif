0ea9718497cf21f19c9179238652650cc82a55c8
awtauer <atauer2015@kellogg.northwestern.edu>
2014-05-18 16:54:11 -0500

    part 3
    3

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,18 @@ class PicturesController < ApplicationController
   	@photo = Picture.find_by({ :id => params[:id] })
   	
   end
+
+  def index
+
+  	@list_of_photos = Picture.all
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rxngif::Application.routes.draw do
+  	get("/create_picture", { :controller => "pictures", :action => "create" })
+  	get("/new_picture_form", { :controller => "pictures", :action => "new" })
   	get("/all_pictures", { :controller => "pictures", :action => "index" })
   	get("/picture_details/:id", { :controller => "pictures", :action => "show" })
-
+	
 
 end
